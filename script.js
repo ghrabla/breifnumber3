44095b22d3bb8bf33fe23555cb0175e128e88492
kamal <kamal@gmail.com>
2021-12-23 19:22:02 +0100

    final de le premier livrabl

---
+++ script.js
@@ -0,0 +1,3 @@
+submit.addEventListener("click", () => {  if(lname.id==""  lname.id==""  email.id==""  subject.id==""  message.id=="")
+{      alert("Vous devez remplire le formulaire")  }
+else{     alert("Message envoyer avec succé")  } });
